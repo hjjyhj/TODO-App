36ac58fbb2a9577f0f6d10c7177c0e4626aa8e76
hjjyhj <95295245+hjjyhj@users.noreply.github.com>
2023-05-22 18:44:21 -0400

    reset 1:03:02

---
+++ client/src/components/ListHeader.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const ListHeader = ({ listName }) => {
+  const signOut = () => {
+    console.log('signout')
+  }
+
+  return (
+    <div className="list-header">
+      <h1>{listName}</h1>
+      <div className="button-container">
+        <button classname="create">ADD NEW</button>
+        <button className="signout" onClick={signOut}>SIGN OUT</button>
+      </div>
+    </div>
+  )
+}
+
+export default ListHeader

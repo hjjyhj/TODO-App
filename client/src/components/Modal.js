daffe58bea32f219e6842d44f09051a42471dbbf
hjjyhj <95295245+hjjyhj@users.noreply.github.com>
2023-05-23 19:25:46 -0400

    1:47:20 working so far

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -4,11 +4,25 @@ const Modal = ({ mode, setShowModal, task}) => {
   const editMode = mode === 'edit' ? true : false
 
   const [data, setData] = useState({
-    user_email: editMode ? task.user_email : null,
+    user_email: editMode ? task.user_email : 'bob@test.com',
     title: editMode ? task.title : null,
     progress: editMode ? task.progress : 50,
     date: editMode ? "" : new Date()
   })
+
+  const postData = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await fetch('http://localhost:8000/todos', {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+      })
+      console.log(response)
+    } catch (err) {
+      console.error(err)
+    }
+  }
 
   const handleChange = (e) => {
     console.log('Changing', e)
@@ -50,7 +64,7 @@ const Modal = ({ mode, setShowModal, task}) => {
             value={data.progress}
             onChange={handleChange}
           />
-          <input className={mode} type ="submit"/>
+          <input className={mode} type ="submit" onClick={editMode ? '' : postData}/>
         </form>
       </div>
     </div>
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const PORT = process.env.PORT ?? 8000
 const express = require('express')
+const {v4: uuidv4} = require('uuid')
 const cors = require('cors')
 const app = express()
 const pool = require('./db')
@@ -9,9 +10,22 @@ app.use(cors())
 // get all todos
 app.get('/todos/:userEmail', async (req, res) => {
     const { userEmail } = req.params
+    const id = uuidv4()
     try {
         const todos = await pool.query('SELECT * FROM todos WHERE user_email = $1', [userEmail])
         res.json(todos.rows)
+    } catch (err) {
+        console.error(err)
+    }
+})
+
+// create new todo
+app.post('./todos', (req, res) => {
+    const { user_email, title, progress, date} = req.body
+    console.log(user_email, title, progress, date)
+    try {
+        pool.query(`INSERT INTO todos(id, user_email, title, progress, date) VALUES($1, $2, $3, $4. $5)`,
+        [id, user_email, title, progress, date])
     } catch (err) {
         console.error(err)
     }

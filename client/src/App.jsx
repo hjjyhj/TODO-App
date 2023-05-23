991c9cda19e266453b67f7b2cdd35c4af768ca22
hjjyhj <95295245+hjjyhj@users.noreply.github.com>
2023-05-22 20:31:43 -0400

    55:47 working so far

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import ListHeader from './components/ListHeader'
 
 const App = () => {
+
+  const getData = async () => {
+    const userEmail = 'johnkimm@umich.edu'
+    try{
+      const response = await fetch(`http://localhost:8000/todos/${userEmail}`)
+      const json = await response.json()
+      console.log(json)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  useEffect(() => getData, [])
   return (
     <div className="app">
       <ListHeader listName={'Holiday tick list'}/>

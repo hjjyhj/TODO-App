7c5d2cc631005b4c3a7d2a5fdf7a7699d650f879
hjjyhj <95295245+hjjyhj@users.noreply.github.com>
2023-05-25 15:21:26 -0400

    Auth 2:11:56

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React, {useEffect, useState} from 'react'
 import ListHeader from './components/ListHeader'
 import ListItem from './components/ListItem'
+import Auth from './components/Auth'
 
 const App = () => {
 
   const userEmail = 'johnkimm@umich.edu'
   const [tasks, setTasks] = useState(null)
+  const authToken = false
 
   const getData = async () => {
     try{
@@ -24,8 +26,12 @@ const App = () => {
   const sortedTasks = tasks?.sort((a, b) => new Date(a.date) - new Date(b.date))
   return (
     <div className="app">
-      <ListHeader listName={'Holiday tick list'} getData={getData}/>
-      {sortedTasks?.map((task) => <ListItem key={task.id} task={task} getData={getData}/>)}
+      {!authToken && <Auth/>}
+      {authToken &&
+        <>
+        <ListHeader listName={'Holiday tick list'} getData={getData}/>
+        {sortedTasks?.map((task) => <ListItem key={task.id} task={task} getData={getData}/>)}
+        </>}
     </div>
   )
 }

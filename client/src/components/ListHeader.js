56d0404a1b4de050cad205ce6918504217c7ab28
hjjyhj <95295245+hjjyhj@users.noreply.github.com>
2023-05-26 14:34:29 -0400

    sign out, log in
    2:41:04

--- client/src/components/ListHeader.js
+++ client/src/components/ListHeader.js
@@ -1,10 +1,16 @@
 import React, {useState} from 'react'
 import Modal from './Modal'
+import {useCookies} from 'react-cookie'
 const ListHeader = ({ listName, getData }) => {
   const [showModal, setShowModal] = useState(false)
+  const [cookies, setCookie, removeCookie] = useCookies(null)
 
   const signOut = () => {
     console.log('signout')
+    removeCookie('Email')
+    removeCookie('AuthToken')
+    window.location.reload()
+
   }
 
   return (
